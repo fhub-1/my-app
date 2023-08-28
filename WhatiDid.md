d0090586c67a5a7bb2680725dc70434a7d0b08c4
fhub-1 <flutterhub1.21.0@gmail.com>
2023-08-29 01:38:14 +0300

    working new no iedea

---
+++ WhatiDid.md
@@ -0,0 +1,6 @@
+# thing i have added to the project
+
+today i have add next aauth and prisma with luide icon just for today
+
+- setting prisma with postsql database
+-
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import Image from 'next/image'
 
-export default function Home() {
+
+export default  function Home() {
+ 
   return (
-    <>
-    <Card className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'>
-      <Card className='md:flex'>
-      <div className="md:shrink-0">
-      <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.DtDXCz5yMJ7j4bpNSWThngHaFj%26pid%3DApi&f=1&ipt=ca9d434e7ab6baa451accf88c43527ed2972a7853535248e97a3fa1d2be96657&ipo=images" alt="Modern building architecture" />
-    </div>
-    <CardHeader>
-      <CardTitle>Card Title</CardTitle>
-      <CardDescription>Card Description</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <p>Card Content</p>
-    </CardContent>
-    <CardFooter>
-      <p>Card Footer</p>
-    </CardFooter>
-    </Card>
-  </Card>
-    </>
+   <main>
+      
+     hello world!
+    </main>
     
   
   )
